36625c3cbcd71008c92b2bf36a196f0e13e3fe4d
Dave <diacovmusic@gmail.com>
2022-04-20 16:21:27 -0400

    commit

--- main.py
+++ main.py
@@ -1,28 +1,5 @@
-from pathlib import Path
+import os
 
-path = Path("hello.py")
+os.chdir('/Users/Dave/Desktop/Pokedex_og_shots')
 
-print(path)
-
-
-# Getting the home directory of the current user
-print(path.home())
-
-print(path.exists())
-
-# Checking whether the path represent the path to a file
-print(path.is_file())
-
-# Checking whether the path represents the path to a directory
-print(path.is_dir())
-
-# suffix of the path
-print(path.suffix)
-
-print(path.stem)
-
-print(path.parent)
-
-# Print the absolute path
-print(path.absolute())
-
+print(os.getcwd())
---
+++ test.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+
+
+path = Path("hello.py")
+
+print(path)
+
+
+# Getting the home directory of the current user
+print(path.home())
+
+print(path.exists())
+
+# Checking whether the path represent the path to a file
+print(path.is_file())
+
+# Checking whether the path represents the path to a directory
+print(path.is_dir())
+
+# suffix of the path
+print(path.suffix)
+
+print(path.stem)
+
+print(path.parent)
+
+# Print the absolute path
+print(path.absolute())
+
